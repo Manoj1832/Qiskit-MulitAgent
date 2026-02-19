4dd0c5cf252faed818e3ac8ff61065862e9b60e7
Prahalya05 <prahalyasivakumar@gmail.com>
2026-02-19 16:23:54 +0530

    test commit

---
+++ extra_file.py
@@ -0,0 +1,12 @@
+#This file is created for testing purposes-index error
+
+def faulty_binary_search(arr, target):
+    left, right = 0, len(arr)   # ERROR: should be len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
